cd8c47100eb8e375600984d58992a7c29b84604a
Asif Arafat Rafat <asif-arafat-rafat@gmail.com>
2024-11-11 09:40:45 +0600

    Day 2_1

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url='https://www.startech.com.bd/lenovo-ideapad-slim-3-15abr8-ryzen-7-7730u-arctic-grey-15-6-inch-fhd-laptop'
+url='https://www.discudemy.com/language/english'
 response=requests.get(url)
 soup=BeautifulSoup(response.text,"lxml")
-print(soup.find_all('table')[1])
+print(soup.find_all('div',class_='description'))
