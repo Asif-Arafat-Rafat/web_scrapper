1dc3676e03013be7209d95ef90d64ab629e7a067
Asif Arafat Rafat <asif-arafat-rafat@gmail.com>
2024-11-11 21:24:48 +0600

    Day 2(List info and Couponed Link)

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url='https://www.discudemy.com/language/english/114'
+url='https://www.discudemy.com/language/english/'
 response=requests.get(url)
 soup=BeautifulSoup(response.text,"lxml")
 desc=soup.find_all('section',class_='card')
@@ -11,10 +11,20 @@ for d in desc:
     price= d.find_all('span',style="float:right;font-size: 20px;")
     link = d.find('a',class_='card-header')
     clink=link['href'] if link else ''
+    if clink:
+        nresp=requests.get(clink)
+    if(nresp.status_code==200):
+        nsoup=BeautifulSoup(nresp.text,'lxml')
+        nlink= nsoup.find('a',class_="ui big inverted green button discBtn")
+        response=requests.get(nlink['href'])
+        soup=BeautifulSoup(response.text,'lxml')
+        link=soup.find('div',class_="ui segment")
+        link=link.find('a')
     if(name and price and clink):
         print(f"\n\n\n\nCourse No:{i}")
         i=i+1
         print(name.text)
         for p in price:
             print(p.text)
-        print(clink)
+        print(f'{link['href']} and {len(link)}') if nlink else ''
+
