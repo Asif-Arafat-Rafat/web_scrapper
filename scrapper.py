a5fa29d14b60fce7c77aa1cb354e5d48c275512c
Asif Arafat Rafat <asif-arafat-rafat@gmail.com>
2024-11-12 22:42:52 +0600

    Day 3 function

--- scrapper.py
+++ scrapper.py
@@ -1,30 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 
-url='https://www.discudemy.com/language/english/'
+def link_ext(url):
+    response = requests.get(url)
+    soup= BeautifulSoup(response.text,'lxml')
+    link1= soup.find('a',class_='ui big inverted green button discBtn')
+    response = requests.get(link1['href'])
+    soup=BeautifulSoup(response.text,'lxml')
+    link=soup.find('div',class_="ui segment")
+    link=link.find('a')
+    print(link['href'])
+i=1
+
+j=1
+run=0
+url=f'https://www.discudemy.com/language/english/{i if i > 1 else ''}'
 response=requests.get(url)
 soup=BeautifulSoup(response.text,"lxml")
 desc=soup.find_all('section',class_='card')
-i=1
 for d in desc:
     name= d.find('div',class_='header')
     price= d.find_all('span',style="float:right;font-size: 20px;")
     link = d.find('a',class_='card-header')
+    image=d.find('img')
+    image=image['src'] if image else ''
     clink=link['href'] if link else ''
-    if clink:
-        nresp=requests.get(clink)
-    if(nresp.status_code==200):
-        nsoup=BeautifulSoup(nresp.text,'lxml')
-        nlink= nsoup.find('a',class_="ui big inverted green button discBtn")
-        response=requests.get(nlink['href'])
-        soup=BeautifulSoup(response.text,'lxml')
-        link=soup.find('div',class_="ui segment")
-        link=link.find('a')
-    if(name and price and clink):
-        print(f"\n\n\n\nCourse No:{i}")
-        i=i+1
-        print(name.text)
+    if(name and price and clink and price != "$0"):
+        print(f"\n\n\n\nCourse No:{j}")
+        j=j+1
+        print(f"Name:{name.text}")
+        print(f'Image:{image}') if image else d
         for p in price:
             print(p.text)
-        print(f'{link['href']} and {len(link)}') if nlink else ''
-
+        link_ext(clink)        
