92474a6468445313df3e25deb7b9f0584bec61a6
Asif Arafat Rafat <asif-arafat-rafat@gmail.com>
2024-11-26 23:59:34 +0600

    Day 4 multi page load

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import re
 def link_ext(url):
     response = requests.get(url)
     soup= BeautifulSoup(response.text,'lxml')
@@ -11,25 +11,26 @@ def link_ext(url):
     link=link.find('a')
     print(link['href'])
 i=1
-
 j=1
-run=0
-url=f'https://www.discudemy.com/language/english/{i if i > 1 else ''}'
-response=requests.get(url)
-soup=BeautifulSoup(response.text,"lxml")
-desc=soup.find_all('section',class_='card')
-for d in desc:
-    name= d.find('div',class_='header')
-    price= d.find_all('span',style="float:right;font-size: 20px;")
-    link = d.find('a',class_='card-header')
-    image=d.find('img')
-    image=image['src'] if image else ''
-    clink=link['href'] if link else ''
-    if(name and price and clink and price != "$0"):
-        print(f"\n\n\n\nCourse No:{j}")
-        j=j+1
-        print(f"Name:{name.text}")
-        print(f'Image:{image}') if image else d
-        for p in price:
-            print(p.text)
-        link_ext(clink)        
+for i in range(110):
+    url=f'https://www.discudemy.com/language/english/{i if i > 1 else ''}'
+    response=requests.get(url)
+    soup=BeautifulSoup(response.text,"lxml")
+    desc=soup.find_all('section',class_='card')
+    for d in desc:
+        name= d.find('div',class_='header')
+        price= d.find_all('span',style="float:right;font-size: 20px;")
+        link = d.find('a',class_='card-header')
+        image=d.find('img')
+        image=image['src'] if image else ''
+        clink=link['href'] if link else ''
+        discounted=re.compile(r"\$\d+\s*->\s*\$0")
+        hasDisc=any(discounted.search(p.text) for p in price)
+        if(name and price and clink and hasDisc):
+            print(f"\n\n\n\nCourse No:{j}")
+            j=j+1
+            print(f"Name:{name.text}")
+            print(f'Image:{image}') if image else d
+            for p in price:
+                print(p.text)
+            link_ext(clink)        
